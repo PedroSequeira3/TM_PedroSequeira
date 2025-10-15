443b195bda77abd68520dcec3bc0d1565178aa45
Pedro Sequeira <125473683+PedroSequeira3@users.noreply.github.com>
2025-10-15 11:11:20 +0100

    Update setupSalmon.sh

--- setupSalmon.sh
+++ setupSalmon.sh
@@ -15,8 +15,7 @@ if [ -z "$TRANSCRIPTOME_FASTA" ]; then
 fi
 
 if [ ! -f "$TRANSCRIPTOME_FASTA" ]; then
-    echo "Error: transcriptome FASTA file not found at 
-$TRANSCRIPTOME_FASTA"
+    echo "Error: transcriptome FASTA file not found at $TRANSCRIPTOME_FASTA"
     exit 1
 fi
 
